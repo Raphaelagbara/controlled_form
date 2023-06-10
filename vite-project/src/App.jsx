98344188ddf737af2415a9e117b315f2dab89019
Raphael Agbara <ralphagbara@email.com>
2023-06-10 15:02:36 +0100

    controlled form components

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,5 @@
+import MessageBox from "./components/MessageBox";
+
+const App = () => <MessageBox nameVal ="John Doe" natureVal="Tech Support" queryVal="I have been having problems with my account, please help me resolve it" />;
+
+export default App;
